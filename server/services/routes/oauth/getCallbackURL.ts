99f4cbf223ded10683f0b21e5e28d2bccd0f474d
Sebola3461 <mixvideostubecontato@gmail.com>
2023-07-26 17:28:43 -0300

    change scope

--- server/services/routes/oauth/getCallbackURL.ts
+++ server/services/routes/oauth/getCallbackURL.ts
@@ -4,6 +4,6 @@ export async function getCallbackURL(_req: Request, res: Response) {
   return res.status(200).send({
     status: 200,
     statusText: "Success",
-    data: `https://osu.ppy.sh/oauth/authorize?client_id=${process.env.OSU_CLIENT_ID}&redirect_uri=${process.env.OSU_CALLBACK_URL}&response_type=code&scope=public`,
+    data: `https://osu.ppy.sh/oauth/authorize?client_id=${process.env.OSU_CLIENT_ID}&redirect_uri=${process.env.OSU_CALLBACK_URL}&response_type=code&scope=identify`,
   });
 }
