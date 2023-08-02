0a8ae8c0d11f34fe03850f2dfb5d85bed969a496
Sebola3461 <mixvideostubecontato@gmail.com>
2023-08-02 17:50:29 -0300

    enable command

--- server/services/discord/commands/index.ts
+++ server/services/discord/commands/index.ts
@@ -2,6 +2,7 @@ import { SlashCommand } from "../models/SlashCommand";
 import verificationEmbed from "./embed/verificationEmbed";
 import grouprole from "./grouprole/grouprole";
 import rankrole from "./rankrole/rankrole";
+import unverifiedRole from "./role/unverifiedRole";
 import verifiedRole from "./role/verifiedRole";
 
 export const DiscordCommands = [
@@ -9,4 +10,5 @@ export const DiscordCommands = [
   rankrole,
   verificationEmbed,
   verifiedRole,
+  unverifiedRole,
 ] as SlashCommand[];
